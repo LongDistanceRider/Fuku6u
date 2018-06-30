dd97771bb9572e05dc1ae181a00c8262e7a6a2e6
梟 <>
2018-06-30 20:36:11 +0900

    人狼役職追加

--- src/main/java/fuku6u/role/Werewolf.java
+++ src/main/java/fuku6u/role/Werewolf.java
@@ -14,14 +14,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Possessed extends AbstractRole {
+/**
+ * 人狼は狂人と同じく占い師COする戦略
+ */
+public class Werewolf extends AbstractRole {
 
     /* 占い結果 */
     Map<Agent, Species> divinedMap = new HashMap<>();
 
     @Override
     public Role getRole() {
-        return Role.POSSESSED;
+        return Role.WEREWOLF;
     }
 
     @Override
