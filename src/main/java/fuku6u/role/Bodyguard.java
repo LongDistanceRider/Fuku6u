d91dfe7fd1863a95aa4329f86aaa62f3112a1117
梟 <>
2018-06-30 20:53:42 +0900

    狩人役職を追加

---
+++ src/main/java/fuku6u/role/Bodyguard.java
@@ -0,0 +1,28 @@
+package fuku6u.role;
+
+import fuku6u.board.BoardSurface;
+import fuku6u.wolfGroupExpectation.WolfGroupExpectation;
+import org.aiwolf.common.data.Role;
+import org.aiwolf.common.net.GameInfo;
+
+public class Bodyguard extends AbstractRole {
+    @Override
+    public Role getRole() {
+        return Role.BODYGUARD;
+    }
+
+    @Override
+    public void dayStart(GameInfo gameInfo, BoardSurface bs, WolfGroupExpectation wExpect) {
+
+    }
+
+    @Override
+    public void talk(BoardSurface boardSurface) {
+
+    }
+
+    @Override
+    public void finish(BoardSurface boardSurface) {
+
+    }
+}
